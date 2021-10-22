19a5da696443828b8c2cfd70a1e16d9108a4c328
minaatta-IOS <minamagdyfci@gmail.com>
2021-10-22 21:55:25 +0200

    project init

---
+++ FoursquareTask/FoursquareTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoursquareTask
+//
+//  Created by Mina on 10/22/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
